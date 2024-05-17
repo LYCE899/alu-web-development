d3e0cbff6516d8eae4f9dbf51fefafc996a17749
LYCE899 <a.coulibaly@alustudent.com>
2024-05-18 01:53:51 +0200

    correct caching

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -1,35 +1,23 @@
-class BaseCaching:
-    """ Base caching class """
-    def __init__(self):
-        self.cache_data = {}
+#!/usr/bin/python3
+"""
+BasicCache class that inherits from BaseCaching
+"""
 
-    def print_cache(self):
-        """ Prints the cache """
-        print("Current cache:")
-        for key in self.cache_data.keys():
-            print("{}: {}".format(key, self.cache_data.get(key)))
+BaseCaching = __import__('base_caching').BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """ Basic cache class inheriting from BaseCaching """
+    """ Define BasicCache """
 
     def put(self, key, item):
-        """ Add an item to the cache """
-        if key is not None and item is not None:
-            self.cache_data[key] = item
+        """ Add key/value pair to the cache """
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
 
     def get(self, key):
-        """ Retrieve an item from the cache """
-        if key is not None:
-            return self.cache_data.get(key)
-        return None
-
-
-# Example usage:
-basic_cache = BasicCache()
-basic_cache.put(1, 'A')
-basic_cache.put(2, 'B')
-basic_cache.put(3, 'C')
-
-print(basic_cache.get(1))  # Output: A
-print(basic_cache.get(4))  # Output: None
+        """ Retrieve value from the cache based on given key """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data.get(key)
+        
